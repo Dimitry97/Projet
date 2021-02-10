25f788bb3d4a95b66527c3fa78628bc762921734
Aymeric <Aymeric@DESKTOP-PIL61QT>
2021-02-10 10:19:41 +0100

    listeenchere2 servlet push

---
+++ src/fr/eni/encheres/servlets/ListeEncheres2Servlet.java
@@ -0,0 +1,39 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListeEncheres2Servlet
+ */
+@WebServlet("/ListeEncheres2Servlet.html")
+public class ListeEncheres2Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListeEncheres2Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/jsp/creerProfil2.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
