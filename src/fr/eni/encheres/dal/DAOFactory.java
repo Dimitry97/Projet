b5ed4a8343f1d05436f0b6b4977f31c6267f57f0
Dimitry <dimitri.germany@gmail.com>
2021-02-10 14:29:08 +0100

    DAOFactory complété

--- src/fr/eni/encheres/dal/DAOFactory.java
+++ src/fr/eni/encheres/dal/DAOFactory.java
@@ -1,5 +1,11 @@
 package fr.eni.encheres.dal;
 
+import fr.eni.encheres.dal.artcileVendu.ArticleVenduDAO;
+import fr.eni.encheres.dal.artcileVendu.ArticleVenduImpl;
+import fr.eni.encheres.dal.categorie.CategorieDAO;
+import fr.eni.encheres.dal.categorie.CategorieImpl;
+import fr.eni.encheres.dal.retrait.RetraitDAO;
+import fr.eni.encheres.dal.retrait.RetraitImpl;
 import fr.eni.encheres.dal.utilisateur.UtilisateurDAO;
 import fr.eni.encheres.dal.utilisateur.UtilisateurImpl;
 
@@ -13,4 +19,29 @@ public class DAOFactory {
 		
 		return utilisateurDAO;
 	}
+	
+	public static ArticleVenduDAO getArticleDAO() {
+		ArticleVenduDAO articleDAO = null;
+		
+		articleDAO = new ArticleVenduImpl();
+		
+		return articleDAO;
+	}
+	
+	public static CategorieDAO getCategorieDAO() {
+		CategorieDAO categorieDAO = null;
+		
+		categorieDAO = new CategorieImpl();
+		
+		return categorieDAO;
+	}
+	
+	public static RetraitDAO getRetraitDAO() {
+		RetraitDAO retraitDAO = null;
+		
+		retraitDAO = new RetraitImpl();
+		
+		return retraitDAO;
+	}
+	
 }
