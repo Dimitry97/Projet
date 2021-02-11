13d93853aa50d103e8ef8c90a7eaba330ae547e7
Aymeric <Aymeric@DESKTOP-PIL61QT>
2021-02-11 09:38:12 +0100

    Push UtilisateurImpl

--- src/fr/eni/encheres/dal/utilisateur/UtilisateurImpl.java
+++ src/fr/eni/encheres/dal/utilisateur/UtilisateurImpl.java
@@ -4,26 +4,26 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.DALException;
 import fr.eni.encheres.dal.DBConnection;
-import fr.eni.encheres.dal.utilisateur.UtilisateurDAO;
 
-public class UtilisateurImpl implements UtilisateurDAO {
-
-	private final static String RECHERCHER = "SELECT pseudo, nom, prenom, email, telephone, rue, code_postal, ville FROM UTILISATEURS WHERE pseudo = '?';";
-	private final static String INSERER = "INSERT INTO UTILISATEURS (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, administrateur)"
-			+ " values ('?','?','?','?','?','?','?','?','?','?','?');";
-	private final static String SUPPRIMER = "DELETE FROM UTILISATEURS WHERE pseudo = '?';";
-	private final static String MODIFIER = " UPDATE UTILISATEURS set pseudo = '?', nom = '?', prenom = '?', email = '?', telephone = '?', rue ='?',"
-			+ " code_postal = '?', ville = '?', mot_de_passe = '?', administrateur = '?' WHERE no_utilisateur = '?'";
-	private final static String PSEUDOMDP = "SELECT * FROM UTILISATEURS WHERE (pseudo = ? OR email = ?) AND mot_de_passe = ?";
-	private final static String SELECTBYNO = "SELECT * FROM UTILISATEURS WHERE no_utilisateur = ?";
-
+public class UtilisateurImpl  implements UtilisateurDAO{
+	
+	
+	private final static String RECHERCHER = "SELECT pseudo, nom, prenom, email, telephone, rue, code_postal, ville FROM UTILISATEURS WHERE pseudo = ?;";
+	private final static String INSERER = "INSERT INTO UTILISATEURS (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) values (?,?,?,?,?,?,?,?,?,?,?);";
+	private final static String SUPPRIMER = "DELETE FROM UTILISATEURS WHERE pseudo = ?;";
+	private final static String  MODIFIER = " UPDATE UTILISATEURS set pseudo = ?, nom = ?, prenom = ?, email = ?, telephone = ?, rue =?,"
+			+ " code_postal = ?, ville = ?, mot_de_passe = ?, administrateur = ? WHERE no_utilisateur = ?";
+	
+	
+	
+	
 	/**
 	 * Methode pour selectionner un utilisateur avec tous les parametres
-	 * 
 	 * @param pseudo
 	 * @return un utilisateur
 	 * @finally libere les connexions ouvertes
@@ -34,45 +34,44 @@ public class UtilisateurImpl implements UtilisateurDAO {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		Utilisateur utilisateur = null;
-
+		
 		cnx = DBConnection.seConnecter();
-
+		
 		try {
 			pstmt = cnx.prepareStatement(RECHERCHER);
 			pstmt.setString(1, pseudo);
 			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				utilisateur = new Utilisateur(rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"),
-						rs.getString("email"), rs.getString("telephone"), rs.getString("rue"),
-						rs.getString("code_postal"), rs.getString("ville"), rs.getString("mot_de_passe"),
-						rs.getInt("credit"), rs.getBoolean("administrateur"));
+			if(rs.next()) {
+				utilisateur = new Utilisateur(rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"),rs.getString("telephone"),
+						rs.getString("rue"), rs.getString("code_postal"), rs.getString("ville"), rs.getString("mot_de_passe"),rs.getInt("credit"), rs.getBoolean("administrateur"));
 			}
 		} catch (SQLException e) {
 			throw new DALException("echec de rechercheProfilAvecMotDePasse");
-		} finally {
+		}finally {
 			DBConnection.seDeconnecter(cnx, pstmt);
 		}
-
+				
 		return utilisateur;
 	}
-
+	
 	/**
 	 * Methode pour inserer un utilisateur
-	 * 
 	 * @param une instance d'utilisateur
 	 * @finally libere les connexions ouvertes
 	 */
 	@Override
 	public void inserer(Utilisateur utilisateur) throws DALException, SQLException {
-
+		
 		Connection cnx = null;
 		PreparedStatement pstmt = null;
-
+		
 		cnx = DBConnection.seConnecter();
 		cnx.setAutoCommit(false);
-
+		
 		try {
-			pstmt = cnx.prepareStatement(INSERER);
+			
+			pstmt = cnx.prepareStatement(INSERER, Statement.RETURN_GENERATED_KEYS);
+			System.out.println("ici");
 			pstmt.setString(1, utilisateur.getPseudo());
 			pstmt.setString(2, utilisateur.getNom());
 			pstmt.setString(3, utilisateur.getPrenom());
@@ -85,51 +84,62 @@ public class UtilisateurImpl implements UtilisateurDAO {
 			pstmt.setInt(10, utilisateur.getCredit());
 			pstmt.setBoolean(11, utilisateur.isAdministrateur());
 
+			System.out.println("pb la");
 			pstmt.executeUpdate();
-
+			
+			ResultSet rs = pstmt.getGeneratedKeys();
+			
+			if(rs.next()) {
+				int noUtilisateur = rs.getInt(1);
+				utilisateur.setNoUtilisateur(noUtilisateur);
+			}
+						
 		} catch (SQLException e) {
 			cnx.rollback();
-			throw new DALException("Probleme methode inserer - " + e);
-		} finally {
+			throw new SQLException("Probleme methode inserer - "+e);
+		}finally {
 			cnx.setAutoCommit(true);
 			DBConnection.seDeconnecter(cnx, pstmt);
 		}
-
+		
+		
+		
+		
 	}
-
 	/**
 	 * Methode pour supprimer un utilisateur
-	 * 
 	 * @param une instance d'utilisateur
 	 * @finally libere les connexions ouvertes
 	 */
-
+	
 	@Override
 	public void supprimer(Utilisateur utilisateur) throws DALException, SQLException {
 		Connection cnx = null;
 		PreparedStatement pstmt = null;
-
+		
 		cnx = DBConnection.seConnecter();
 		cnx.setAutoCommit(false);
-
+		
 		try {
 			pstmt = cnx.prepareStatement(SUPPRIMER);
 			pstmt.setString(1, utilisateur.getPseudo());
-
+			
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			cnx.rollback();
 			throw new DALException("erreur suppression utilisateur");
-		} finally {
+		}finally {
 			cnx.setAutoCommit(true);
 			DBConnection.seDeconnecter(cnx, pstmt);
 		}
-
+		
+		
+		
+		
 	}
-
+	
 	/**
 	 * Methode pour modifier un utilisateur
-	 * 
 	 * @param une instance d'utilisateur
 	 * @finally libere les connexions ouvertes
 	 */
@@ -137,10 +147,10 @@ public class UtilisateurImpl implements UtilisateurDAO {
 	public void modifier(Utilisateur utilisateur) throws DALException, SQLException {
 		Connection cnx = null;
 		PreparedStatement pstmt = null;
-
+		
 		cnx = DBConnection.seConnecter();
 		cnx.setAutoCommit(false);
-
+		
 		try {
 			pstmt = cnx.prepareStatement(MODIFIER);
 			pstmt.setString(1, utilisateur.getPseudo());
@@ -162,71 +172,7 @@ public class UtilisateurImpl implements UtilisateurDAO {
 			cnx.setAutoCommit(true);
 			DBConnection.seDeconnecter(cnx, pstmt);
 		}
-
-	}
-	
-	public Utilisateur getUtilisateurPseudoMdp(String login, String motDePasse) throws DALException {
-		Utilisateur unUtilisateur = new Utilisateur();
-		Connection cnx = null;
-		PreparedStatement pstmt = null;
-
-		cnx = DBConnection.seConnecter();
 		
-		try {
-			 pstmt = cnx.prepareStatement(PSEUDOMDP);
-			 pstmt.setString(1, login);
-			 pstmt.setString(2, login);
-			 pstmt.setString(3, motDePasse);
-            ResultSet rs = pstmt.executeQuery();
-
-            while(rs.next())
-            {
-            	unUtilisateur = getUtilisateurByNo(rs.getInt("no_utilisateur"));
-            }
-		}
-		catch (SQLException e) {
-			throw new DALException("Erreur recherche mot de passe et pseudo");
-		} finally {
-			DBConnection.seDeconnecter(cnx, pstmt);
-		}
-		
-		
-		return unUtilisateur;
-	}
-	
-	public Utilisateur getUtilisateurByNo(int no) throws DALException {
-		Utilisateur unUtilisateur = new Utilisateur();
-		Connection cnx = null;
-		PreparedStatement pstmt = null;
-
-		cnx = DBConnection.seConnecter();
-		
-		try {
-			pstmt = cnx.prepareStatement(SELECTBYNO);
-			pstmt.setInt(1, no);
-            ResultSet rs = pstmt.executeQuery();
-            
-            while(rs.next())
-            {
-            	unUtilisateur = creerUtilisateur(rs);
-            }
-		}
-		catch (SQLException e) {
-			throw new DALException("echec de récupération utilisateur");
-		} finally {
-			DBConnection.seDeconnecter(cnx, pstmt);
-		}
-		
-		return unUtilisateur;
 	}
 
-	public Utilisateur creerUtilisateur(ResultSet rs) throws SQLException {
-
-		Utilisateur resultat = new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo"), rs.getString("nom"),
-				rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"), rs.getString("rue"),
-				rs.getString("code_postal"), rs.getString("ville"), rs.getString("mot_de_passe"), rs.getInt("credit"),
-				rs.getBoolean("administrateur"));
-
-		return resultat;
-	}
 }
