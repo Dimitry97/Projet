ad8fb1180a77da2370366ae3ecc504be52963ee7
Aymeric <Aymeric@DESKTOP-PIL61QT>
2021-02-11 18:06:07 +0100

    Push  méthode  dans utilisateurimpl supprimée

--- src/fr/eni/encheres/dal/utilisateur/UtilisateurImpl.java
+++ src/fr/eni/encheres/dal/utilisateur/UtilisateurImpl.java
@@ -18,7 +18,7 @@ public class UtilisateurImpl  implements UtilisateurDAO{
 	private final static String SUPPRIMER = "DELETE FROM UTILISATEURS WHERE pseudo = ?;";
 	private final static String  MODIFIER = " UPDATE UTILISATEURS set pseudo = ?, nom = ?, prenom = ?, email = ?, telephone = ?, rue =?,"
 			+ " code_postal = ?, ville = ?, mot_de_passe = ?, administrateur = ? WHERE no_utilisateur = ?";
-	
+	private final static String SELECTPSEUDOANDMDP = "SELECT pseudo, mot_de_passe FROM UTILISATEURS WHERE pseudo = ? AND mot_de_passe = ?";
 	
 	
 	
@@ -239,4 +239,29 @@ public class UtilisateurImpl  implements UtilisateurDAO{
 		return unique;
 	
 	}
+	
+	@Override
+	public void getUtilisateurPseudoMdp(String pseudo, String motDePasse) throws DALException {
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Utilisateur utilisateur = null;
+		
+		cnx = DBConnection.seConnecter();
+		try {
+			pstmt = cnx.prepareStatement(SELECTPSEUDOANDMDP);
+			pstmt.setString(1, pseudo);
+			pstmt.setString(2, motDePasse);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				utilisateur = new Utilisateur(rs.getString("pseudo"), rs.getString("mot_de_passe"));
+			}
+		} catch (SQLException e) {
+			throw new DALException("echec de rechercheProfilAvecMotDePasse");
+		}finally {
+			DBConnection.seDeconnecter(cnx, pstmt);
+		}
+				
+	}
+
 }
