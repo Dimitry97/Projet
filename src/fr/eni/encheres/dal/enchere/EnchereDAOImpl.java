d5825429aaad81f4da9be16da9ab2c38aea53795
Dimitry <dimitri.germany@gmail.com>
2021-02-15 14:15:15 +0100

    Enchere DAO Impl

--- src/fr/eni/encheres/dal/enchere/EnchereDAOImpl.java
+++ src/fr/eni/encheres/dal/enchere/EnchereDAOImpl.java
@@ -32,25 +32,25 @@ public class EnchereDAOImpl implements EnchereDAO {
 		Connection cnx = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		
-        try {
-        	cnx = DBConnection.seConnecter();
-        	cnx.setAutoCommit(false);
+
+		try {
+			cnx = DBConnection.seConnecter();
+			cnx.setAutoCommit(false);
 			stmt = cnx.prepareStatement(INSERT, PreparedStatement.RETURN_GENERATED_KEYS);
-			
-			  stmt.setDate(1, Methodes.dateJavaVersSql(enchere.getDateEnchere()));
-		        stmt.setInt(2, enchere.getMontantEnchere());
-		        stmt.setInt(3, enchere.getArticleVendu().getNoArticle());
-		        stmt.setInt(4, enchere.getEncherisseur().getNoUtilisateur());
-		        stmt.setBoolean(5, enchere.isRemporte());
-		       
-		        stmt.executeUpdate();
-		        
-		        rs = stmt.getGeneratedKeys();
-		        
-		        if (rs.next()) {
-					enchere.setIdEnchere(rs.getInt(1));
-		        }
+
+			stmt.setDate(1, Methodes.dateJavaVersSql(enchere.getDateEnchere()));
+			stmt.setInt(2, enchere.getMontantEnchere());
+			stmt.setInt(3, enchere.getArticleVendu().getNoArticle());
+			stmt.setInt(4, enchere.getEncherisseur().getNoUtilisateur());
+			stmt.setBoolean(5, enchere.isRemporte());
+
+			stmt.executeUpdate();
+
+			rs = stmt.getGeneratedKeys();
+
+			if (rs.next()) {
+				enchere.setIdEnchere(rs.getInt(1));
+			}
 		} catch (SQLException e) {
 			cnx.rollback();
 			throw new DALException("Erreur lors de l'ajout du l'enchere ");
@@ -58,37 +58,115 @@ public class EnchereDAOImpl implements EnchereDAO {
 			cnx.setAutoCommit(true);
 			DBConnection.seDeconnecter(cnx, stmt);
 		}
-        
-      
-		}
+
+	}
 
 	@Override
 	public List<Enchere> getAll() throws DALException {
 		List<Enchere> list = new ArrayList<>();
 		Connection cnx = null;
-		cnx = DBConnection.seConnecter();
-		
-		 
-	
+		PreparedStatement stmt = null;
+
+		try {
+			cnx = DBConnection.seConnecter();
+			stmt = cnx.prepareStatement(GET_ALL);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				Enchere enchere = new Enchere();
+				enchere.setIdEnchere(rs.getInt("no_enchere"));
+				enchere.setDateEnchere(Methodes.dateSQLVersJava(rs.getDate("date_enchere")));
+				enchere.setMontantEnchere(rs.getInt("montant_enchere"));
+				enchere.setArticleVendu(DAOFactory.getArticleDAO().selectArticleByNo(rs.getInt("no_article")));
+				// enchere.setEncherisseur(DAOFactory.getById(rs.getInt("no_utilisateur")));
+				enchere.setRemporte(rs.getBoolean("remporte"));
+
+				list.add(enchere);
+			}
+		} catch (SQLException e) {
+			throw new DALException("Erreur sélection enchère");
+		} finally {
+			DBConnection.seDeconnecter(cnx, stmt);
+		}
+
 		return list;
 	}
 
 	@Override
 	public void update(Enchere enchere) throws DALException {
-		// TODO Auto-generated method stub
+		Connection cnx = null;
+		PreparedStatement stmt = null;
+		try {
+			cnx = DBConnection.seConnecter();
+			stmt = cnx.prepareStatement(UPDATE);
+			stmt.setDate(1, Methodes.dateJavaVersSql(enchere.getDateEnchere()));
+			stmt.setInt(2, enchere.getMontantEnchere());
+			stmt.setInt(3, enchere.getArticleVendu().getNoArticle());
+			stmt.setInt(4, enchere.getEncherisseur().getNoUtilisateur());
+			stmt.setBoolean(5, enchere.isRemporte());
+			stmt.setInt(6, enchere.getIdEnchere());
+
+			stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DALException("Erreur modification enchère");
+		} finally {
+			DBConnection.seDeconnecter(cnx, stmt);
+		}
 
 	}
 
 	@Override
 	public void delete(int id) throws DALException {
-		// TODO Auto-generated method stub
+		Connection cnx = null;
+		PreparedStatement stmt = null;
+		try {
+			cnx = DBConnection.seConnecter();
+			stmt = cnx.prepareStatement(DELETE);
+			
+			stmt.setInt(1, id);
 
+			stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DALException("Erreur suppression enchère");
+		} finally {
+			DBConnection.seDeconnecter(cnx, stmt);
+		}
+
+		
 	}
 
 	@Override
 	public List<Enchere> getByEncherisseur(int id) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Enchere> list = new ArrayList<>();
+		Connection cnx = null;
+		PreparedStatement stmt = null;
+		
+		try { 
+			cnx = DBConnection.seConnecter();
+			stmt = cnx.prepareStatement(GET_BY_ENCHERISSEUR);
+			 stmt.setInt(1, id);
+			 ResultSet rs = stmt.executeQuery();
+			 
+			while (rs.next()) {
+				Enchere enchere = new Enchere();
+				enchere.setIdEnchere(rs.getInt("no_enchere"));
+				enchere.setDateEnchere(Methodes.dateSQLVersJava(rs.getDate("date_enchere")));
+				enchere.setMontantEnchere(rs.getInt("montant_enchere"));
+				enchere.setArticleVendu(DAOFactory.getArticleDAO().selectArticleByNo(rs.getInt("no_article")));
+				//enchere.setEncherisseur(utilisateurDAO.getById(rs.getInt("no_utilisateur")));
+				enchere.setRemporte(rs.getBoolean("remporte"));
+				
+				list.add(enchere);
+			}
+			
+		} catch (SQLException e) {
+			throw new DALException("Erreur enchère");
+		} finally {
+			DBConnection.seDeconnecter(cnx, stmt);
+		}
+		return list;
 	}
 
 	@Override
